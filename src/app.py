777e6e7ff644290acf4338b3c425c8bcf0332b6c
Sergi Castillo Tiñena <sergicastillo@hotmail.es>
2024-05-09 20:34:37 +0200

    D4

--- src/app.py
+++ src/app.py
@@ -1,12 +1,6 @@
-"""
-Module for launch application
-"""
-
 import asyncio
-
 from prometheus_client import start_http_server
 from application.app import SimpleServer
-
 
 class Container:
     """
@@ -20,10 +14,18 @@ class Container:
         """Function for start server"""
         await self._simple_server.run_server()
 
-
 if __name__ == "__main__":
+    # Iniciar un servidor HTTP para exponer métricas Prometheus en el puerto 8000
     start_http_server(8000)
+    
+    # Instanciar la clase Container
     container = Container()
+    
+    # Obtener el bucle de eventos asyncio
     loop = asyncio.get_event_loop()
+    
+    # Iniciar el servidor FastAPI en el bucle de eventos asyncio
     asyncio.ensure_future(container.start_server(), loop=loop)
+    
+    # Ejecutar el bucle de eventos asyncio
     loop.run_forever()
