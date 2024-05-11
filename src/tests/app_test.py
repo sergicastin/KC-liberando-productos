7a4ab6dd50aa42bb88203e4deffc556da4a46797
Sergi Castillo Tiñena <sergicastillo@hotmail.es>
2024-05-11 03:05:16 +0200

    Update app_test.py

--- src/tests/app_test.py
+++ src/tests/app_test.py
@@ -1,11 +1,7 @@
-"""
-Module used for testing simple server module
-"""
-
 from fastapi.testclient import TestClient
 import pytest
 
-from application.app import app
+from ..app import app  # Ajusta la importación al nuevo lugar de tu app
 
 client = TestClient(app)
 
@@ -14,15 +10,15 @@ class TestSimpleServer:
     TestSimpleServer class for testing SimpleServer
     """
     @pytest.mark.asyncio
-    async def read_health_test(self):
+    async def test_read_health(self):  # Ajusta el nombre del método de prueba
         """Tests the health check endpoint"""
-        response = client.get("health")
+        response = client.get("/health")  # Añade "/" al inicio del endpoint
 
         assert response.status_code == 200
         assert response.json() == {"health": "ok"}
 
     @pytest.mark.asyncio
-    async def read_main_test(self):
+    async def test_read_main(self):  # Ajusta el nombre del método de prueba
         """Tests the main endpoint"""
         response = client.get("/")
 
@@ -30,7 +26,7 @@ class TestSimpleServer:
         assert response.json() == {"msg": "Hello World"}
 
     @pytest.mark.asyncio
-    async def read_bye_test(self):
+    async def test_read_bye(self):  # Ajusta el nombre del método de prueba
         """Tests the bye endpoint"""
         response = client.get("/bye")
 
